ecff6efd0c3cdfd3b12fd5c5ad4f3f91256c9d11
Br3c4Developer <br3c4developer@gmail.com>
2025-11-27 20:14:39 -0300

    Exception handling - findById

---
+++ src/main/java/com/br3c4dev/course/services/ResourceNotFoundException/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.br3c4dev.course.services.ResourceNotFoundException;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+	
+}
